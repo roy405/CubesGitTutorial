6384d782493c49acb179780e67df5ac525f47ced
Kawshik Roy <kawshik.zero@yahoo.com>
2023-08-28 23:43:26 +1000

    70% progress (C)

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class LoadAssets : MonoBehaviour 
 {
+    [SerializeField] private GameObject blueObj;
     public GameObject redObj;
     // Start is called before the first frame update
     void Start()
     {
 
+        Application.targetFrameRate = 60;
+
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
+
+        
     }
 
     // Update is called once per frame
