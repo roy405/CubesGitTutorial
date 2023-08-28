1e2d1f346acf5eb1c4f338d27cd71782cbb1a093
Kawshik Roy <kawshik.zero@yahoo.com>
2023-08-28 23:55:39 +1000

    80% Complete (D)

---
+++ Assets/Scripts/PrintAndHide.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour
+{
+    public Renderer rend;
+    private int i = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        i++;
+        Debug.Log(gameObject.name + "=" + i);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++;
+        Debug.Log(gameObject.name + "=" + i);
+    }
+}
